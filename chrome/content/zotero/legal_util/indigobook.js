335709d971f86ae8241040f4cbdc50e82617865b
David Stein <davidbstein@gmail.com>
2025-05-09 13:51:41 -0400

    add some indigobook stubs

---
+++ chrome/content/zotero/legal_util/indigobook.js
@@ -0,0 +1,19 @@
+/**
+ * IndigoBook citation generator utility
+ * This file provides functions for generating IndigoBook citations
+ */
+
+"use strict";
+
+var Zotero_IndigoBook = {
+	/**
+	 * Generate an IndigoBook citation for the given item
+	 * 
+	 * @param {Zotero.Item} item - The Zotero item to generate a citation for
+	 * @return {string} The IndigoBook citation as a string
+	 */
+	generateCitation: function(item) {
+		// This is a placeholder that will be replaced with actual citation logic
+		return `TODO (${item.getField('title')})`;
+	}
+};
